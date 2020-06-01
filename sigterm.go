45c6677db1b2fac89a3b4375bcd8ed64d0982c84
Alexander F. Rødseth <rodseth@gmail.com>
2020-06-01 16:16:56 +0200

    Show a status message for each signal

--- sigterm.go
+++ sigterm.go
@@ -12,14 +12,57 @@ import (
 func (e *Editor) SetUpTerminateHandler(c *vt100.Canvas, status *StatusBar, tty *vt100.TTY) {
 	sigChan := make(chan os.Signal, 1)
 
-	// Clear any previous terminate handlers
-	signal.Reset(syscall.SIGTERM)
+	for msg, sig := range map[string]os.Signal{
+		"Abort":     syscall.SIGABRT,
+		"Alarm":     syscall.SIGALRM,
+		"Bus":       syscall.SIGBUS,
+		"Child":     syscall.SIGCHLD,
+		"CLD":       syscall.SIGCLD,
+		"Continue":  syscall.SIGCONT,
+		"FPE":       syscall.SIGFPE,
+		"Hangup":    syscall.SIGHUP,
+		"ILL":       syscall.SIGILL,
+		"Interrupt": syscall.SIGINT,
+		"IO":        syscall.SIGIO,
+		"IOT":       syscall.SIGIOT,
+		"Kill":      syscall.SIGKILL,
+		"Pipe":      syscall.SIGPIPE,
+		"POLL":      syscall.SIGPOLL,
+		"PROF":      syscall.SIGPROF,
+		"PWR":       syscall.SIGPWR,
+		"QUIT":      syscall.SIGQUIT,
+		"SEGV":      syscall.SIGSEGV,
+		"STKFLT":    syscall.SIGSTKFLT,
+		"STOP":      syscall.SIGSTOP,
+		"SIGSYS":    syscall.SIGSYS,
+		"ctrl-c":    syscall.SIGTERM,
+		"TRAP":      syscall.SIGTRAP,
+		"TSTP":      syscall.SIGTSTP,
+		"TTIN":      syscall.SIGTTIN,
+		"TTOU":      syscall.SIGTTOU,
+		"UNUSED":    syscall.SIGUNUSED,
+		"URG":       syscall.SIGURG,
+		"USR1":      syscall.SIGUSR1,
+		"USR2":      syscall.SIGUSR2,
+		"VTALRM":    syscall.SIGVTALRM,
+		"WINCH":     syscall.SIGWINCH,
+		"XCPU":      syscall.SIGXCPU,
+		"XFSZ":      syscall.SIGXFSZ,
+	} {
 
-	signal.Notify(sigChan, syscall.SIGTERM)
-	go func() {
-		for range sigChan {
-			status.SetMessage("ctrl-c")
-			status.Show(c, e)
-		}
-	}()
+		sig := sig
+		msg := msg
+
+		// Clear any previous terminate handlers
+		//signal.Reset(sig)
+
+		signal.Notify(sigChan, sig)
+		go func() {
+			for range sigChan {
+				status.SetMessage(msg)
+				status.Show(c, e)
+			}
+		}()
+
+	}
 }
